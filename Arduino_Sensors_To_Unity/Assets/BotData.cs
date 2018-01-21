13a78569a08be78db87ac331b2493aad0ff8e7e0
Phoenix Perry <contact@phoenixperry.com>
2018-01-21 15:56:41 +0000

    adding our unity world!

---
+++ Arduino_Sensors_To_Unity/Assets/BotData.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotData :MonoBehaviour
+{
+    //this class deals with all bot data 
+    public static string name;
+    public static ArrayList compass = new ArrayList();
+    public static int btn;
+    private static string[] sensors;
+
+
+    public void updateData(string values)
+    {
+        compass.Clear();
+        sensors = values.Split(' '); //split the array at every space. we use a space to deliminate our values from Arduino 
+        name = sensors[0]; //get which bot we're dealing with, which is saved in the 0 position 
+                            // Debug.Log(name);
+        compass.Add(sensors[1]);
+        compass.Add(sensors[2]);
+        compass.Add(sensors[3]);
+        compass.Add(sensors[4]);
+        int.TryParse(sensors[5], out btn);
+        Debug.Log("Bot Parsed: " + name + " btn " + btn + "Compass vals" + compass[0] + " " + compass[1] + " " + compass[2] + " " + compass[3]);
+    }
+
+
+    
+}
---
+++ Arduino_Sensors_To_Unity/Assets/SerialReader.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+using System.Collections.Generic;
+
+
+public class SerialReader : MonoBehaviour
+{
+
+    SerialPort stream = new SerialPort("COM3", 9600);
+    bool firstTime = true;
+    public GameObject botData;
+    public GameObject touchData; 
+    // Use this for initialization
+    void Start()
+    {
+        stream.Open();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        stream.ReadTimeout = 40; // this is a safety catch in case the port hangs
+        try
+        {
+            string value = stream.ReadLine();
+            string[] sensors = value.Split(' ');
+            //foreach (string s in sensors)
+            //{ 
+            //   if(s != "") 
+            //        Debug.Log(s);
+                 
+            //}
+       
+            //if we have 2 values, we have a touch state 
+            if (sensors.Length == 2)
+            {
+                touchData.GetComponent<BotData>().updateData(value);
+            }
+            else if (sensors.Length == 6) {
+                botData.GetComponent< BotData > ().updateData(value);
+          
+           }
+
+            stream.BaseStream.Flush();
+        }
+        catch (System.Exception e) {
+            //Debug.Log("your serial port shit the bed. try unplugging your arduino and reloading your code on it"); 
+        }
+ 
+   }
+}
